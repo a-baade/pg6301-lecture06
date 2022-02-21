a51f471dee91adc45d733b9968162624327a41ad
a-baade <andreasbaade@hotmail.com>
2022-02-21 10:57:32 +0100

    Move useLoader and fetchJSON to separate files

--- client/index.js
+++ client/index.js
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { fetchJSON } from "./http";
+import { useLoader } from "./useLoader";
 
 function LoginLinks() {
   return (
@@ -16,29 +18,6 @@ function LoginLinks() {
       </ul>
     </div>
   );
-}
-
-async function fetchJSON(url) {
-  const res = await fetch(url);
-  return await res.json();
-}
-
-function useLoader(loadingFn) {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState();
-  const [data, setData] = useState();
-  useEffect(async () => {
-    setLoading(true);
-    try {
-      setData(await loadingFn());
-    } catch (error) {
-      setError(error);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
-
-  return { loading, error, data };
 }
 
 function Frontpage() {
