d1e68e2135e89c6313b596a8ceb86cf42a4be881
a-baade <andreasbaade@hotmail.com>
2022-02-21 10:53:50 +0100

    Refactor custom hooks and fetch

--- client/index.js
+++ client/index.js
@@ -1,22 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
+function LoginLinks() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to={"/login"}> Login </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to={"/register"}> Register </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+async function fetchJSON(url) {
+  const res = await fetch(url);
+  return await res.json();
+}
+
+function useLoader(loadingFn) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [data, setData] = useState();
+  useEffect(async () => {
+    setLoading(true);
+    try {
+      setData(await loadingFn());
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  return { loading, error, data };
+}
+
 function Frontpage() {
+  const { loading, error, data } = useLoader(
+    async () => await fetchJSON("api/login")
+  );
+  const user = data;
+
+  if (loading) {
+    return <div>LOADING...</div>;
+  }
+  if (error) {
+    return (
+      <div
+        style={{ border: "1px solid red", background: "Pink", width: "30%" }}
+      >
+        An error occurred: {error.toString()}
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Movie Application</h1>
-      <div>
-        <ul>
-          <li>
-            <Link to={"/login"}> Login </Link>
-          </li>
-          <li>
-            {" "}
-            <Link to={"/register"}> Register </Link>
-          </li>
-        </ul>
-      </div>
+      {user ? (
+        <div>
+          {user.fullName} ({user.username})
+        </div>
+      ) : (
+        <LoginLinks />
+      )}
     </div>
   );
 }
