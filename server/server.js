4b8c06ecf47b76554972d8188597c5a7129e487f
a-baade <andreasbaade@hotmail.com>
2022-02-21 09:11:52 +0100

    add pre-commit hooks via husky & prettier

--- server/server.js
+++ server/server.js
@@ -3,5 +3,5 @@ import express from "express";
 const app = express();
 
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`server started at http://localhost:${server.address().port}`);
-})
+  console.log(`server started at http://localhost:${server.address().port}`);
+});
