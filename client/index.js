621dc56e5ac527873a79bb3ee247fa434dc62ee3
a-baade <andreasbaade@hotmail.com>
2022-02-21 09:43:43 +0100

    server react from express

--- client/index.js
+++ client/index.js
@@ -1,4 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("app"));
+function Frontpage() {
+  return (
+    <div>
+      <h1>Movie Application</h1>
+      <div>
+        <ul>
+          <li>
+            <Link to={"/login"}> Login </Link>
+          </li>
+          <li>
+            {" "}
+            <Link to={"/register"}> Register </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function Login() {
+  return <h1>Hello User</h1>;
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Frontpage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
