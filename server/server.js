89a339ca1465bd2585074dec34d024f48b046d8a
a-baade <andreasbaade@hotmail.com>
2022-02-21 12:38:01 +0100

    Rough JSON post method

--- server/server.js
+++ server/server.js
@@ -1,14 +1,51 @@
 import express from "express";
 import * as path from "path";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+const users = [
+  {
+    username: "admin",
+    fullName: "Noen Andre Persson",
+    password: "secret123",
+  },
+];
+
+app.use((req, res, next) => {
+  const { username } = req.signedCookies;
+  req.user = users.find((u) => u.username === username);
+  next();
+});
 
 app.get("/api/login", (req, res) => {
-  if (req.user) {
-    const { username, fullName } = req.user;
-    return res.json({ username, fullName });
+  function respond() {
+    if (req.user) {
+      const { username, fullName } = req.user;
+      return res.json({ username, fullName });
+    } else {
+      res.sendStatus(204);
+    }
+  }
+  setTimeout(respond, 400);
+});
+
+app.post("api/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+  if (user) {
+    res.cookie("username", user.username, { signed: true });
+    res.sendStatus(200);
   } else {
-    res.sendStatus(204);
+    res.sendStatus(401);
   }
 });
 
